f202ddd851975fe0ec5626a9d155622b2f69db1c
Prasanna Swaminathan <prasanna@beeswax.com>
2018-03-27 18:19:54 -0400

    fix doc

--- bnakr/app.js
+++ bnakr/app.js
@@ -12,10 +12,16 @@ function bnakrModTable(tbody) {
 			outerDate = date;
 		}
 		var typ = rows[i].getElementsByClassName('type-toggle')[0];
-		if (typ.textContent === 'ATM transaction' && date !== 'Mar 26, 2018') {
-			var desc = rows[i].getElementsByClassName('DDAdescription')[0],
-				amt = rows[i].getElementsByClassName('NUMSTR')[0];
-			var amtNum = parseFloat(amt.textContent.replace('−$', ''));
+		if (typ.textContent !== 'ATM transaction' && typ.textContent !== 'Fee' || date === 'Mar 26, 2018') {
+			continue;
+		}
+		var desc = rows[i].getElementsByClassName('DDAdescription')[0],
+			amt = rows[i].getElementsByClassName('NUMSTR')[0];
+		var amtNum = parseFloat(amt.textContent.replace('−$', ''));
+		if (typ.textContent === 'Fee') {
+			desc.textContent = 'APPLE PAY - SENT MONE 877-233-8552 CA';
+			typ.textContent = 'Debit card transaction';
+		} else {
 			desc.textContent = 'GOLDMAN SACHS BA COLLECTION 000300002391122 WEB ID: 0124085260';
 			typ.textContent = 'ACH debit';
 			if (amtNum % 50 !== 0) {
